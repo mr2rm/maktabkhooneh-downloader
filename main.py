8865902d0614a0cb77ae68dd07a7d2291ad97651
MohamadReza <mohammadreza.rahmani89@gmail.com>
2020-02-15 17:37:09 +0330

    Envrironment variables added to project

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import os
 import requests
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
 
-url = 'https://maktabkhooneh.org/course/%D8%B3%D8%A7%D8%AE%D8%AA%D9%85%D8%A7%D9%86-%D8%AF%D8%A7%D8%AF%D9%87-%D9%87%D8%A7-mk118/'
-session_id = 'yn92x7lck1u5ire119t1u2rkpjy46vhr'
-course_name = 'Data Structure'
+load_dotenv(verbose=True)
 
-base_dir = f'/home/rahmani/Downloads/{course_name}'
-os.makedirs(base_dir, exist_ok=True)
+course_path = os.path.join(os.getenv('base_dir'), os.getenv('course_name'))
+os.makedirs(course_path, exist_ok=True)
 
-response = requests.get(url)
+response = requests.get(os.getenv('course_url'))
 soup = BeautifulSoup(response.content, 'html.parser')
 unit_elements = soup.find_all('a', attrs={'class': 'chapter__unit'})
 
@@ -18,14 +17,14 @@ for i, unit in enumerate(unit_elements, start=1):
     print(f'### Downloading {filename} ###')
 
     unit_path = unit['href']
-    response = requests.get(f'https://maktabkhooneh.org{unit_path}', headers={
-        'Cookie': f'sessionid={session_id};'
+    response = requests.get(f"{os.getenv('base_url')}{unit_path}", headers={
+        'Cookie': f"sessionid={os.getenv('session_id')};"
     })
     soup = BeautifulSoup(response.content, 'html.parser')
     download_link = soup.find('div', attrs={'class': 'unit-content--download'}).find('a')['href']
-    
+
     response = requests.get(download_link)
 
-    path = os.path.join(base_dir, filename)
+    path = os.path.join(course_path, filename)
     with open(path, 'wb') as f:
         f.write(response.content)
