f21fb126c53f28059f21dea989f9c1b0559c558a
MohamadReza <mohammadreza.rahmani89@gmail.com>
2020-02-25 18:59:55 +0330

    Add some command line arguments

--- download.py
+++ download.py
@@ -9,28 +9,37 @@ from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 from tqdm import trange
 
-CONTINUE = False
+BASE_URL = 'https://maktabkhooneh.org'
+
+course_url = None
+course_name = None
+resume = False
 
 
 def download_course():
-    course_path = os.path.join(os.getenv('base_dir'), os.getenv('course_name'))
+    global course_url, course_name, resume
+
+    response = requests.get(course_url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    unit_elements = soup.find_all('a', attrs={'class': 'chapter__unit'})
+    if not course_name:
+        course_name = soup.find('h1', attrs={'class': 'course-content__title'}).text
+
+    course_path = os.path.join(os.getenv('base_dir'), course_name)
     os.makedirs(course_path, exist_ok=True)
 
-    response = requests.get(os.getenv('course_url'))
-    soup = BeautifulSoup(response.content, 'html.parser')
-    unit_elements = soup.find_all('a', attrs={'class': 'chapter__unit'})
-
     progress_bar_size = len(unit_elements)
-    for i in trange(progress_bar_size, desc=os.getenv('course_name'), mininterval=0):
+    for i in trange(progress_bar_size, desc=course_name, mininterval=0):
         filename = f'{i + 1:02d}.mp4'
         path = os.path.join(course_path, filename)
 
-        if CONTINUE and os.path.isfile(path):
+        if resume and os.path.isfile(path):
             continue
 
         print(f'### Downloading {filename} ###')
         unit_path = unit_elements[i]['href']
-        response = requests.get(f"{os.getenv('base_url')}{unit_path}", headers={
+        response = requests.get(f"{BASE_URL}{unit_path}", headers={
             'Cookie': f"sessionid={os.getenv('session_id')};"
         })
         soup = BeautifulSoup(response.content, 'html.parser')
@@ -42,10 +51,14 @@ def download_course():
 
 
 if __name__ == '__main__':
-    opts, argv = getopt(sys.argv[1:], 'r', ['resume'])
+    opts, args = getopt(sys.argv[1:], 'l:n:r', ['link=', 'name=', 'resume'])
     for opt, arg in opts:
-        if opt in ('-r', '--resume'):
-            CONTINUE = True
+        if opt in ('-l', '--link'):
+            course_url = arg
+        elif opt in ('-n', '--name'):
+            course_name = arg
+        elif opt in ('-r', '--resume'):
+            resume = True
 
     load_dotenv(verbose=True)
     download_course()
